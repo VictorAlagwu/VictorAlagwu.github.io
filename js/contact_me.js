da41d703a6a97fe7773d5bfab87bbe2aec2dc09c
Victor Alagwu <victoralagwu@gmail.com>
2017-04-28 00:11:33 +0100

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././php/contact_me.php",
+                url: "https://formspree.io/victoralagwu@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
