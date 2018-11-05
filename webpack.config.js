96337a214cb2ef5756b5633e43f831a5ebaeef72
VictorAlagwu <victoralagwu@gmail.com>
2018-11-05 23:01:56 +0100

    Deploy

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/docs/',
+    publicPath: '/dist/',
     filename: 'build.js'
   },
   module: {
